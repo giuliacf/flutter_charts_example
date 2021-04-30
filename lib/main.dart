4d50192a1ada0747f31665c936dd5e564efe689a
Ana Lemos <anaapaulalemos@gmail.com>
2021-04-30 15:21:18 -0300

    Adicionar named routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter_charts_example/screens/login_screen.dart';
 import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:responsive_framework/utils/scroll_behavior.dart';
 
-import 'screens/login_screen.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -14,23 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Plataforma',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.dark,
-      ),
-      debugShowCheckedModeBanner: false,
-      builder: (context, widget) => ResponsiveWrapper.builder(
-          ClampingScrollWrapper.builder(context, widget),
-          defaultScale: true,
-          minWidth: 450,
-          defaultName: MOBILE,
-          breakpoints: [
-            ResponsiveBreakpoint.resize(450, name: MOBILE),
-            ResponsiveBreakpoint.resize(700, name: TABLET),
-            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-          ]),
-      home: LoginScreen(),
-    );
+        title: 'Plataforma',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        debugShowCheckedModeBanner: false,
+        builder: (context, widget) => ResponsiveWrapper.builder(
+              ClampingScrollWrapper.builder(context, widget),
+              defaultScale: true,
+              minWidth: 450,
+              defaultName: MOBILE,
+              breakpoints: [
+                ResponsiveBreakpoint.resize(450, name: MOBILE),
+                ResponsiveBreakpoint.resize(700, name: TABLET),
+                ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+              ],
+            ),
+        // home: LoginScreen(),
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => LoginScreen(),
+          '/home': (context) => HomeScreen(),
+        });
   }
 }
